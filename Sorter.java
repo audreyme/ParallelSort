e1c37cca67c876fa1d2c6e7832edacdbc3346c26
Audrey Meadows <s592714@sbuniv.edu>
2018-05-10 00:09:39 +0000

    Stardate 025.36.78945. The algotithm is coming along quite nicely. The team assigned to work with it reported back saying they needed to implement merge functionality, though they reported knowing how to implement the algorithm. Higgins, the commanding officer, said he was unsure on how to create connectivity between what has been created and the merge functionality. I'll have to discuss this with HQ. Perhaps they'll have some insight into what this all means. Meadows out.

--- Sorter.java
+++ Sorter.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 import java.io.*;
 import java.util.Random;
 import java.lang.Math;
+
+//Filling the newArray
+
+
 class Sorter
 {
   public static void main(String [] args)
@@ -15,7 +19,7 @@ class Sorter
 
     Random rand = new Random();
 
-
+    System.out.println("The unsorted array");
     for(int i = 0; i < 100; i++)
     {
       array[i] = rand.nextInt(10) + 1;
@@ -36,8 +40,15 @@ class Sorter
     Threadly t1 = new Threadly(0, 49, array, range, min, max);
     Threadly t2 = new Threadly(50, 99, array, range, min, max);
     //start threads
+
+    t1.start();
+    t2.start();
     //join threads
+//    t1.join();
+//    t2.join();
+
     //merge thread data
+
     //print array data
   }
 
@@ -64,25 +75,44 @@ class Threadly extends Thread
   public void run()
   {
     //make an array with the length of the range
-      int [] counterArray = new int[range];
+      int [] counterArray = new int[range + 1];
       int [] newArray = new int[array.length];
       int tracker = 0;
+      int offset;
     //go through data array and counter sort the dooblie doo where index 0 represents min, 1 represents min + 1, all the way to n being max
-      for(int j = 0; j < array.length; j++)
+      for(int j = startIndex; j < endIndex; j++)
       {
         int value = array[j];
-        counterArray[value - min] = counterArray[value - min]++;
-        System.out.println(counterArray[value - min]);
+        //System.out.println("value: " + value + "   min: " + min); Works just fine
+
+        counterArray[value - min ] = counterArray[value - min] + 1;
+        System.out.println("index: "+ (value - min) +"   data value :" +counterArray[value - min]);
       }
     //go through counterArray, and fill in the number of values in newArray
       for(int k = 0; k < counterArray.length; k++)
       {
         int valueNum = counterArray[k];
-        for(int m = tracker; m < tracker+valueNum; m++)
-        {
-          //using the offset, fill valueNum number of instances of (k + min) in newArray
-        }
+        tracker = tracker + valueNum;
 
+        for(int m = tracker - valueNum; m < tracker; m++)
+        {
+    //using the offset, fill valueNum number of instances of (k + min) in newArray, using tracker offset to index
+          newArray[m] = k + min;
+          System.out.println(newArray[m]);
+        }
       }
+
   }//run
+
+}
+//working under the assumption that the two counter arrays are being passed into the method
+merge(int[] counter1, int [] counter2)
+{
+  int[] sortedArray = new int[counter1.length + counter2.length];
+  //get number of values for each index from counter1 & counter2
+  //add them together
+  //iterate through sorted Array similarly to the run() function
+  //repeat for all indexes in the counter1
+
+  return sortedArray;
 }
