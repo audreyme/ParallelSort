470b5670809e54291541315785a0f36e7707ff1d
Audrey Meadows <s592714@sbuniv.edu>
2018-05-10 19:14:19 +0000

    This program successfully sorts an array populated by random integers ranging from 1 - 10

--- Sorter.java
+++ Sorter.java
@@ -12,10 +12,14 @@ class Sorter
   public static void main(String [] args)
   {
     //make an array to be sorted
+    int tracker = 0;
     int min = 11;
     int max = -1;
     int range;
     int [] array = new int[100];
+    int [] newArray = new int[100];
+    int [] counterArray1 = new int[50];
+    int [] counterArray2 = new int[50];
 
     Random rand = new Random();
 
@@ -37,19 +41,34 @@ class Sorter
     range = max - min;
     //create threads
 
-    Threadly t1 = new Threadly(0, 49, array, range, min, max);
-    Threadly t2 = new Threadly(50, 99, array, range, min, max);
+    Threadly t1 = new Threadly(0, 49, array, range, min, max, counterArray1);
+    Threadly t2 = new Threadly(50, 99, array, range, min, max, counterArray2);
     //start threads
 
     t1.start();
     t2.start();
     //join threads
-//    t1.join();
-//    t2.join();
+    try
+    {
+      t1.join();
+      t2.join();
+    } catch(Exception e)
+    {
+      System.out.println("join(s) has/have failed");
+    }
 
-    //merge thread data
-
-    //print array data
+  //merge thread data
+    //get number of values for each index from counter1 & counter2
+    for(int index = 0; index < counterArray1.length; index++)
+    {
+      int instances = counterArray1[index] + counterArray2[index];
+      tracker = tracker + instances;
+      for(int p = tracker - instances; p < tracker; p++)
+      {
+        newArray[p] = index + 1;
+        System.out.println(newArray[p]);
+      }
+    }
   }
 
 }
@@ -59,10 +78,12 @@ class Threadly extends Thread
   int startIndex;
   int endIndex;
   int array[];
+  int counterArray[];
   int range;
   int min;
   int max;
-  Threadly(int startIndex, int endIndex, int [] array, int range, int min, int max)
+
+  Threadly(int startIndex, int endIndex, int [] array, int range, int min, int max, int [] counterArray)
   {
     this.startIndex = startIndex;
     this.endIndex = endIndex;
@@ -70,13 +91,15 @@ class Threadly extends Thread
     this.range = range;
     this.min = min;
     this.max = max;
+    this.counterArray = counterArray;
+
   }//constructor
 
   public void run()
   {
     //make an array with the length of the range
-      int [] counterArray = new int[range + 1];
-      int [] newArray = new int[array.length];
+
+      int [] newArray = new int[array.length];//    MOVE TO THE MAIN
       int tracker = 0;
       int offset;
     //go through data array and counter sort the dooblie doo where index 0 represents min, 1 represents min + 1, all the way to n being max
@@ -88,25 +111,12 @@ class Threadly extends Thread
         counterArray[value - min ] = counterArray[value - min] + 1;
         System.out.println("index: "+ (value - min) +"   data value :" +counterArray[value - min]);
       }
-    //go through counterArray, and fill in the number of values in newArray
-      for(int k = 0; k < counterArray.length; k++)
-      {
-        int valueNum = counterArray[k];
-        tracker = tracker + valueNum;
-
-        for(int m = tracker - valueNum; m < tracker; m++)
-        {
-    //using the offset, fill valueNum number of instances of (k + min) in newArray, using tracker offset to index
-          newArray[m] = k + min;
-          System.out.println(newArray[m]);
-        }
-      }
-
+/*
   }//run
 
 }
 //working under the assumption that the two counter arrays are being passed into the method
-merge(int[] counter1, int [] counter2)
+/*merge(int[] counter1, int [] counter2)
 {
   int[] sortedArray = new int[counter1.length + counter2.length];
   //get number of values for each index from counter1 & counter2
@@ -126,3 +136,4 @@ merge(int[] counter1, int [] counter2)
 
   return sortedArray;
 }
+*/
